e0d5c500c9925be74ddebd385702c112ac92e067
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-07-14 16:24:52 +0300

    navbar

---
+++ lang/ru.js
@@ -0,0 +1,9 @@
+export default {
+    app_title: "Andorable",
+    nav: {
+      shows: "Shows",
+      blog: "Blog",
+      about: "About",
+    },
+    // ...all other en-CA translations
+  }
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <header>
+            <Navbar />
+        </header>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
