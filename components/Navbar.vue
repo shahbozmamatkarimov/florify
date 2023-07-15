d8ee452db47613f89993b6347b9f8239a13d6135
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-07-15 17:58:24 +0300

    home page form

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="navbar container mx-auto xl:px-40 md:px-10 px-5 mt-[70px]">
-    <form class="grid grid-cols-5 sm:py-5 py-3 sm:gap-6 gap-2">
-      <div class="flex items-center relative md:col-span-2 col-span-4">
-        <div
-          class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none"
-        >
-          <img class="location" src="../assets/svg/location.svg" alt="" />
+    <div class="grid grid-cols-5 sm:py-5 py-3 sm:gap-6 gap-2">
+      <form class="items-center grid md:grid-cols-3 grid-cols-5 sm:gap-6 gap-2 relative md:col-span-3 col-span-5">
+        <div class="flex items-center relative md:col-span-2 col-span-4">
+          <div
+            class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none"
+          >
+            <img class="location" src="../assets/svg/location.svg" alt="" />
+          </div>
+          <input
+            type="text"
+            id="input-group-1"
+            class="border md:h-14 h-10 border-gray-300 placeholder-gray-800 md:text-lg rounded-xl outline-none block w-full pl-12 p-2.5"
+            placeholder="Укaжитe адрес доставки"
+            required
+          />
         </div>
-        <input
-          type="text"
-          id="input-group-1"
-          class="border md:h-14 h-10 border-gray-300 placeholder-gray-800 md:text-lg rounded-xl outline-none block w-full pl-12 p-2.5"
-          placeholder="Укaжитe адрес доставки"
-        />
-      </div>
-      <button
-        class="locate md:h-14 h-10 text-center whitespace-nowrap flex items-center justify-center bg-[#5C0099] text-white rounded-xl"
-      >
-        Показать
-      </button>
-      <div class="relative md:col-span-2 col-span-full w-full">
+        <button
+          class="locate md:h-14 h-10 text-center whitespace-nowrap flex items-center justify-center bg-[#5C0099] text-white rounded-xl"
+        >
+          Показать
+        </button>
+      </form>
+      <form class="relative md:col-span-2 col-span-full w-full">
         <input
           type="search"
           class="border md:h-14 h-10 border-gray-300 placeholder-gray-800 md:text-lg rounded-xl outline-none block w-full p-2.5"
@@ -32,8 +35,8 @@
         >
           <i class="bx bx-search text-[#545454] sm:text-2xl text-lg"></i>
         </button>
-      </div>
-    </form>
+      </form>
+    </div>
     <div>
       <ul class="flex gap-6 py-3 md:pr-0 pr-10 overflow-hidden overflow-x-auto">
         <li
@@ -79,7 +82,8 @@
     font-size: 12px;
   }
 
-  input, .locate {
+  input,
+  .locate {
     height: 32px;
     border-radius: 10px;
   }
@@ -90,8 +94,8 @@
 }
 
 @media (max-width: 350px) {
-  .locate{
+  .locate {
     font-size: 9px;
-  }  
+  }
 }
 </style>
